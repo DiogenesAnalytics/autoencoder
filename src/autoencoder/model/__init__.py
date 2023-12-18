420d6689f5aa6c96db2ac2be310db5855b744990
Diogenes Analytics <diogenesanalytics@gmail.com>
2023-12-18 14:35:56 -0500

    Implemented deep autoencoder (#13)

--- src/autoencoder/model/__init__.py
+++ src/autoencoder/model/__init__.py
@@ -1,5 +1,7 @@
 """Implemented autoencoder models."""
-__all__ = ["MinimalAutoencoder", "MinimalParams"]
+__all__ = ["DeepAutoencoder", "DeepParams", "MinimalAutoencoder", "MinimalParams"]
 
+from .deep import DeepAutoencoder
+from .deep import DeepParams
 from .minimal import MinimalAutoencoder
 from .minimal import MinimalParams
---
+++ src/autoencoder/model/deep.py
@@ -0,0 +1,51 @@
+"""Getting deep into autoencoders."""
+from dataclasses import dataclass
+from typing import Any
+from typing import ClassVar
+from typing import Dict
+from typing import Optional
+
+from keras import layers
+
+from .base import BaseAutoencoder
+from .base import BaseModelParams
+from .base import Decode
+from .base import Encode
+from .base import Inputs
+from .base import MetaLayer
+
+
+@dataclass
+class DeepParams(BaseModelParams):
+    """Model layer parameters class for deep autoencoder."""
+
+    # default values from: https://blog.keras.io/building-autoencoders-in-keras.html
+    default_parameters: ClassVar[Dict[str, MetaLayer]] = {
+        "l0": Inputs(layers.InputLayer, {"input_shape": (784,)}),
+        "l1": Encode(layers.Dense, {"units": 128, "activation": "relu"}),
+        "l2": Encode(layers.Dense, {"units": 64, "activation": "relu"}),
+        "l3": Encode(layers.Dense, {"units": 32, "activation": "relu"}),
+        "l4": Decode(layers.Dense, {"units": 64, "activation": "relu"}),
+        "l5": Decode(layers.Dense, {"units": 128, "activation": "relu"}),
+        "l6": Decode(layers.Dense, {"units": 784, "activation": "sigmoid"}),
+    }
+
+    # setup instance layer params
+    l0: Optional[Dict[str, Any]] = None
+    l1: Optional[Dict[str, Any]] = None
+    l2: Optional[Dict[str, Any]] = None
+    l3: Optional[Dict[str, Any]] = None
+    l4: Optional[Dict[str, Any]] = None
+    l5: Optional[Dict[str, Any]] = None
+    l6: Optional[Dict[str, Any]] = None
+
+
+class DeepAutoencoder(BaseAutoencoder):
+    """Getting deep into autoencoders."""
+
+    _default_config = DeepParams()
+
+    def __init__(self, model_config: Optional[DeepParams] = None) -> None:
+        """Overrided base constructor to set the layer params class used."""
+        # call super
+        super().__init__(model_config=model_config)
